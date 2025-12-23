3e3d6ab1fcf48f5581f850be134a0ad90c0d0784
Alexandre Neto <alexandrejorgeneto21@gmail.com>
2025-12-23 18:37:49 +0000

    int__reasons foi alterada para guardar a lista das razões

--- models/intermediate/int__reasons.sql
+++ models/intermediate/int__reasons.sql
@@ -11,18 +11,18 @@ with
 
     , joined_tables as (
         select
-            stg_reasons.reason_id 
-            , stg_order_reasons.order_id
-            , stg_reasons.reason_name
-            , stg_reasons.reason_type
+            stg_order_reasons.order_id
+            , collect_set(stg_reasons.reason_name) as sales_reason
+            , collect_set(stg_reasons.reason_type) as sales_reason_type
         from stg_order_reasons
         left join stg_reasons on 
             stg_order_reasons.reason_id = stg_reasons.reason_id
+        group by 1
     )
 
     , create_sk as (
         select
-            {{ dbt_utils.generate_surrogate_key(['order_id', 'reason_id']) }} as reason_sk
+            {{ dbt_utils.generate_surrogate_key(['order_id', 'sales_reason']) }} as reason_sk
             , *
         from joined_tables
     )
